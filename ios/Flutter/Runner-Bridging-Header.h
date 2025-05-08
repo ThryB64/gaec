b72fc4bbff944e3dfb4ae1805ae8079a5280e460
ThryB64 <thierryber64@gmail.com>
2025-05-08 12:53:10 +0200

    Reset iOS folder with fresh Flutter create

--- ios/Flutter/Runner-Bridging-Header.h
+++
@@ -1,6 +0,0 @@
-#import "GeneratedPluginRegistrant.h"
-#import <Firebase/Firebase.h>
-#import <FirebaseCore/FirebaseCore.h>
-#import <FirebaseFirestore/FirebaseFirestore.h>
-#import <FirebaseAuth/FirebaseAuth.h>
-#import <FirebaseStorage/FirebaseStorage.h> 
